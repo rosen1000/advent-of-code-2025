3644594872ff32efa1c2fcfb57b493fa57b6d753
rosen1000 <rosen.ruseff@gmail.com>
2025-12-04 18:39:21 +0200

    improved utils; grid now accepts generics and cell parser; new getCardinal func

--- utils.ts
+++ utils.ts
@@ -1,34 +1,50 @@
-export class Grid {
-	data: number[][];
+export class Grid<T = string> {
+	data: T[][];
 	size: Vec2;
 
-	constructor(data: string) {
-		this.data = data.split('\n').map((line) => line.split('').map(Number));
+	constructor(data: string, parser: (v: string) => T = (v) => v as T) {
+		this.data = data.split('\n').map((line) => line.split('').map(parser));
 		this.size = new Vec2(this.data.length, this.data[0].length);
 	}
 
-	get(pos: Vec2): number | null {
+	get(pos: Vec2): T | null {
 		if (this.oob(pos)) return null;
 		return this.data[pos.x][pos.y];
 	}
 
-	getAdj(pos: Vec2, dir: Vec2): number | null {
+	getRel(pos: Vec2, dir: Vec2): T | null {
 		let npos = pos.add(dir);
 		return this.get(npos);
 	}
 
+	/**
+	 * Return array of cardinally adjecent positions
+	 * ```txt
+	 * 012
+	 * 3x4
+	 * 567
+	 * ```
+	 */
+	getCardinal(pos: Vec2): (T | null)[] {
+		let positions = [Dir.upleft, Dir.up, Dir.upright, Dir.left, Dir.right, Dir.downleft, Dir.down, Dir.downright];
+		return positions.map((v) => this.getRel(pos, v));
+	}
+
 	/** @returns {boolean} True if setting was a success */
-	set(pos: Vec2, val: number): boolean {
+	set(pos: Vec2, val: T): boolean {
 		if (this.oob(pos)) return false;
 		this.data[pos.x][pos.y] = val;
 		return true;
 	}
 
+	/**
+	 * Out Of Bounds checker
+	 */
 	oob(pos: Vec2): boolean {
 		return pos.x < 0 || pos.y < 0 || pos.x >= this.size.x || pos.y >= this.size.y;
 	}
 
-	forEach(iter: (value: number, pos: Vec2) => void) {
+	forEach(iter: (value: T, pos: Vec2) => void) {
 		for (let i = 0; i < this.size.x; i++) {
 			for (let j = 0; j < this.size.y; j++) {
 				let pos = new Vec2(i, j);
@@ -36,6 +52,10 @@ export class Grid {
 			}
 		}
 	}
+}
+
+export function vec2(x: number, y: number) {
+	return new Vec2(x, y);
 }
 
 export class Vec2 {
@@ -81,4 +101,8 @@ export const Dir = {
 	down: new Vec2(1, 0),
 	left: new Vec2(0, -1),
 	right: new Vec2(0, 1),
+	upleft: new Vec2(-1, -1),
+	upright: new Vec2(-1, 1),
+	downleft: new Vec2(1, -1),
+	downright: new Vec2(1, 1),
 } as const;
